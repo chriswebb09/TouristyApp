0d6620c64c2bc5f113723f189b69b41b70037fc0
chriswebb09 <christopher.webb@flatironschool.com>
2017-02-12 05:08:56 -0500

    Added CLLocation delegate and MGMap delegates'

---
+++ Touristy/Touristy/TourSpotAnnotationView.swift
@@ -0,0 +1,9 @@
+//
+//  TourSpotAnnotationView.swift
+//  Touristy
+//
+//  Created by Christopher Webb-Orenstein on 2/12/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
