9c92f45bbb196308abb281110e1cd411f993241a
chriswebb09 <christopher.webb@flatironschool.com>
2017-02-20 05:12:26 -0500

    update

--- Touristy/Touristy/Application/Data/DataDefaults.swift
+++ Touristy/Touristy/Application/Data/DataDefaults.swift
@@ -1,9 +1,18 @@
 import UIKit
 import CoreLocation
 
-
 struct DataDefaults {
-    // DataDefaults 
+    
+    let defaults = UserDefaults.standard
+    
+    func hasLoggedIn() {
+        let hasLoggedIn = defaults.bool(forKey: "hasLoggedIn")
+        let user = defaults.data(forKey: "currentUser")
+        if hasLoggedIn {
+            print(defaults.description)
+            print(defaults.value(forKey: "hasLoggedIn").debugDescription)
+        }
+    }
 }
 
 
