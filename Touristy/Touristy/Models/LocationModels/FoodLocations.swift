dee0276e59b5d118de82e7ff55ae41bcf0f0e365
chriswebb09 <christopher.webb@flatironschool.com>
2017-02-20 05:04:45 -0500

    update

---
+++ Touristy/Touristy/Models/LocationModels/FoodLocations.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLocations.swift
+//  Touristy
+//
+//  Created by Christopher Webb-Orenstein on 2/20/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+struct FoodLocation {
+    var establishmentName: String
+    var cuisineType: String
+    var address: String
+    var location: CLLocation
+}
