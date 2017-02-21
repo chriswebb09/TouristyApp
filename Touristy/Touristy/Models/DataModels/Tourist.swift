1961dd081a1a0a37991a64c5fbad8add9d0e5eec
chriswebb09 <christopher.webb@flatironschool.com>
2017-02-21 11:04:52 -0500

    added realm

--- Touristy/Touristy/Models/DataModels/Tourist.swift
+++ Touristy/Touristy/Models/DataModels/Tourist.swift
@@ -2,13 +2,30 @@ import UIKit
 import Realm
 import RealmSwift
 
-class Tourist: Object {
+
+class RealmImage: Object {
+    required init() {
+        super.init()
+    }
     
+    required init(value: Any, schema: RLMSchema) {
+        fatalError("init(value:schema:) has not been implemented")
+    }
+    
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        fatalError("init(realm:schema:) has not been implemented")
+    }
+}
+
+class Tourist: Object {
+    dynamic var username: String = ""
+    dynamic var emailAddress: String = ""
+    dynamic var uuid: String = ""
     dynamic var tourPartyName: String = ""
     dynamic var tourType: String = ""
     dynamic var distanceTravelled: String = ""
     dynamic var triviaScore: Int = 0
-    var pictureTaken: List = List()
+    var picturesTaken: List<RealmImage> = List()
     dynamic var levelOfActivity: String = ""
     dynamic var distanceToTravel: String = ""
     
@@ -23,6 +40,22 @@ class Tourist: Object {
     
     required init(value: Any, schema: RLMSchema) {
         super.init(value: value, schema: schema)
+    }
+    
+    init(username: String, emailAddress: String, uuid: String, tourPartyName: String,
+         tourType: String, distanceTravelled: String, triviaScore: Int, picturesTaken: List<RealmImage>,
+         levelOfActivity: String, distanceToTravel: String) {
+        self.username = username
+        self.emailAddress = emailAddress
+        self.uuid = uuid
+        self.tourPartyName = tourPartyName
+        self.tourType = tourType
+        self.distanceToTravel = distanceToTravel
+        self.triviaScore = triviaScore
+        self.picturesTaken = picturesTaken
+        self.levelOfActivity = levelOfActivity
+        self.distanceToTravel = distanceToTravel
+        super.init()
     }
     
     static func getPicturesFrom(data: Data) -> UIImage {
